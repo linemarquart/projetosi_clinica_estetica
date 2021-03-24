90214b92a0eb174fb5a58feee335bd7c365be699
Marcos Paulo Simões Donizete <51121525+MarcospsDonizete@users.noreply.github.com>
2021-03-23 21:01:49 -0300

    Delete Clinica/src/main/java/com/fatec/repository directory

--- Clinica/src/main/java/com/fatec/repository/FichaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fatec.repository;
-
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-import com.fatec.model.Ficha;
-
-public interface FichaRepository extends JpaRepositoryImplementation<Ficha, Long> {
-
-}
